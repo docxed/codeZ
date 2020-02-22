7bc0e563f90d567e8e4948b0cb6098c36d98e330
Akira <54892411+docxed@users.noreply.github.com>
2020-02-22 16:52:54 +0700

    committed

--- Lab6-4 [Reverse Word EZ].c
+++ Lab6-4 [Reverse Word EZ].c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-  
-int main() {
-  char word[100];
-  scanf("%[^\n]s", word);
-  for (int i=strlen(word)-1; i>=0; --i){
-    printf("%c", word[i]);
-  }
-  return 0;
+
+int main()
+{
+    char word[100];
+    scanf("%[^\n]", word);
+    for (int i = strlen(word) - 1; i >= 0; --i)
+    {
+        printf("%c", word[i]);
+    }
+    return 0;
 }
--- Lab6-7 [Encrypt EZ].c
+++ Lab6-7 [Encrypt EZ].c
@@ -1,41 +1,56 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
- 
-int main() {
-  char code[] = {'a', 'b', 'x', 'y', 'p', 'q', 'r', 'm', 'n', 'c', 'e', 'd', 'k', 'l', 'j', 'o', 's', 'h', 't', 'u', 'f', 'v', 'z', 'g', 'w', 'i'};
-  char raw[200], word;
-  int i, j, get_index, encrypt, encrypt_cycling;
-  scanf("%[^\n]", raw);
-  for (i=0; i<strlen(raw); ++i){
-    if(!isalpha(raw[i])){
-      printf(" ");
-    }else if (isupper(raw[i])){
-      word = tolower(raw[i]);
-      for (j=0; j<26; ++j){
-        if (word == code[j]){
-          get_index = j;
-          break;
+
+int main()
+{
+    char code[] = {'a', 'b', 'x', 'y', 'p', 'q', 'r', 'm', 'n', 'c', 'e', 'd', 'k', 'l', 'j', 'o', 's', 'h', 't', 'u', 'f', 'v', 'z', 'g', 'w', 'i'};
+    char raw[200], word;
+    int i, j, get_index, encrypt, encrypt_cycling;
+    scanf("%[^\n]", raw);
+    for (i = 0; i < strlen(raw); ++i)
+    {
+        if (!isalpha(raw[i]))
+        {
+            printf(" ");
         }
-      }
-      encrypt = (j-5);
-      if (encrypt < 0) encrypt_cycling = 26 + encrypt;
-      else encrypt_cycling = encrypt;
-      printf("%c", toupper(code[encrypt_cycling]));
-    }else{
-      word = raw[i];
-      for (j=0; j<26; ++j){
-        if (word == code[j]){
-          get_index = j;
-          break;
+        else if (isupper(raw[i]))
+        {
+            word = tolower(raw[i]);
+            for (j = 0; j < 26; ++j)
+            {
+                if (word == code[j])
+                {
+                    get_index = j;
+                    break;
+                }
+            }
+            encrypt = (j - 5);
+            if (encrypt < 0)
+                encrypt_cycling = 26 + encrypt;
+            else
+                encrypt_cycling = encrypt;
+            printf("%c", toupper(code[encrypt_cycling]));
         }
-      }
-      encrypt = (j-5);
-      if (encrypt < 0) encrypt_cycling = 26 + encrypt;
-      else encrypt_cycling = encrypt;
-      printf("%c", code[encrypt_cycling]);
+        else
+        {
+            word = raw[i];
+            for (j = 0; j < 26; ++j)
+            {
+                if (word == code[j])
+                {
+                    get_index = j;
+                    break;
+                }
+            }
+            encrypt = (j - 5);
+            if (encrypt < 0)
+                encrypt_cycling = 26 + encrypt;
+            else
+                encrypt_cycling = encrypt;
+            printf("%c", code[encrypt_cycling]);
+        }
     }
-  }
- 
-  return 0;
+
+    return 0;
 }
